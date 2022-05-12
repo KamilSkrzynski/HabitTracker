a777715e78ee235af3054b22ba95b2290be3519b
KamilSkrzynski <kamilskrzynski95@gmail.com>
2022-05-12 16:36:30 +0200

    Initial commit

---
+++ HabitTracker/HabitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackerApp.swift
+//  HabitTracker
+//
+//  Created by Kamil Skrzyński on 12/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
